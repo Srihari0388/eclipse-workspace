4271b85be6549cd2e3c22a6f6be915fb2bf5a5d0
Srihari Babu Kolla <sriharibabu.kolla@gmail.com>
2024-10-14 22:01:16 +0530

    new

---
+++ AddNumbers.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class AddNumbers {
+public static void main( String args[]){
+System.out.println(21+40);
+System.out.println(5);
+System.out.println("Srihari"+"Babu");
+System.out.println(8 + "\n" + 13 + " " + 21 + "\n");
+
+}
+}
---
+++ Selinium/src/javaPackage/Squares.java
@@ -0,0 +1,11 @@
+public class Squares{
+public static void main(String[] args){
+	int i,n;
+	for(i=1;i<=5;i++)
+	{
+		n=i*i;
+		System.out.println(i+" "+"-"+" "+n);
+	}
+
+}
+}
